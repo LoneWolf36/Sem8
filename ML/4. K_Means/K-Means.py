a32bc4d9612b719753529102ad785a0f02db07a8
Faizan Khan <runefaizan36@gmail.com>
2019-04-25 01:09:39 +0530

    Added K-Means

--- ML/4. K_Means/K-Means.py
+++ ML/4. K_Means/K-Means.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import copy
-
+import matplotlib.pyplot as plt
 df = pd.read_csv("Data.csv")
 
 np.random.seed(200)
@@ -26,7 +26,6 @@ def assignment(df, centroids):
     df['closest'] = df['closest'].map(lambda x: int(x.lstrip('distance_from_')))
     return df
 
-df = assignment(df, centroids)
 #print(df)
 
 def update(k):
@@ -35,25 +34,36 @@ def update(k):
         centroids[i][1] = np.mean(df[df['closest'] == i]['y'])
     return k
 
+df = assignment(df, centroids)
 centroids = update(centroids)
 
 while True:
     closest_centroids = df['closest'].copy(deep=True)
-    centroids = update(centroids)
     df = assignment(df, centroids)
+    centroids = update(centroids)
     if closest_centroids.equals(df['closest']):
         break
 
 print(df['closest'][5])
 print(centroids)
 
+plt.scatter(df['x'] , df['y'] , c=df['closest'])
+plt.show()
+
 #using sklearn
 from sklearn.cluster import KMeans
+df = pd.read_csv("Data.csv")
 
-kmeans = KMeans(n_clusters=2)
+init_cluster = np.ndarray( shape=(2,2), dtype=float , buffer=np.array( [[0.1,0.6], [0.3,0.2]] ) )
+
+kmeans = KMeans(init=init_cluster, n_clusters=2)
 kmeans.fit(df)
 labels = kmeans.predict(df)
 centroids = kmeans.cluster_centers_
 
+plt.scatter(df['x'] , df['y'] , c=kmeans.labels_)
+plt.show()
+
+
 print(labels)
-print(centroids)
+print(kmeans.cluster_centers_)
