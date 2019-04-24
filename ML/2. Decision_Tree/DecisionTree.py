2d030e201f37c5436b31ff21881f969f95273a8b
Faizan Khan <runefaizan36@gmail.com>
2019-04-25 01:06:33 +0530

    Added KNN

--- ML/2. Decision_Tree/DecisionTree.py
+++ ML/2. Decision_Tree/DecisionTree.py
@@ -1,13 +1,13 @@
+import graphviz
+from sklearn import preprocessing
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 import os
 os.environ["PATH"] += os.pathsep + "D:\Program Files (x86)\Graphviz\Bin"
-import numpy as np
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-from sklearn import tree
-from sklearn import preprocessing
-import graphviz
 
 # 1. Reading dataset
 data = pd.read_csv("d:\Documents\Sem8\ML\Assignment 2\Ass2data.csv", sep=',')
@@ -67,3 +67,13 @@ dot_data2 = tree.export_graphviz(clf_entropy, out_file=None, feature_names=featu
                                  class_names=class_names, filled=True, rounded=True, special_characters=True)
 graph = graphviz.Source(dot_data2)
 graph.render('DecisionTreeEntropy', view=True)
+
+
+"""
+Entropy measures impurity in data
+Information gain is which class splits the data the best i.e. describes it the best
+More IG = < Entropy
+
+Gini Index as our cost function used to evaluate splits in the dataset.
+A Gini score gives an idea of how good a split is by how mixed the classes are in the two groups created by the split. A perfect separation results in a Gini score of 0, whereas the worst case split that results in 50/50 classes.
+"""
