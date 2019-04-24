a1f2aaf8e57619cdcd0b8ea5a7cda658d768905c
Faizan Khan <runefaizan36@gmail.com>
2019-04-24 23:06:26 +0530

    Added and updated Linear Regression, Added Decision Tree

--- ML/1. Linear_Regression/linear_regression.py
+++ ML/1. Linear_Regression/linear_regression.py
@@ -1,6 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import linear_model, metrics
+from sklearn.model_selection import train_test_split
+
+
+def compute_error_for_y(y):
+    totalError = 0
+    mean_y = 0
+    for i in range(len(y)):
+        mean_y += y[i]
+    mean_y = mean_y/len(y)
+
+    for i in range(len(y)):
+        totalError += ((y[i] - mean_y) ** 2)
+
+    return totalError
 
 
 def compute_error_for_given_points(b, m, x, y):
@@ -29,9 +44,9 @@ def step_gradient_descent(x, y, b_current, m_current, learning_rate):
     m_new = m_current - (learning_rate * m_gradient)
     return (b_new, m_new)
 
-def main():
+
+def manual_linear_regression(data):
     # Step 1 : Get data
-    data = pd.read_csv("data_back_pain.csv")
     x = data.iloc[:, 0]
     y = data.iloc[:, 1]
 
@@ -42,11 +57,29 @@ def main():
     number_of_itr = 1000
 
     # Step 3 : Train the model
-    print('Starting gradient descent at b={0}, m={1} with initial error={2}'.format(
+    print('Starting gradient descent at b = {0}, m = {1} with initial error = {2}'.format(
         initial_b, initial_m, compute_error_for_given_points(initial_b, initial_m, x, y)))
+
+    # Mean squared error initially
+    mean_squared_error = compute_error_for_given_points(
+        initial_b, initial_m, x, y)
+
+    mean_squared_error_y = compute_error_for_y(y)
+
+    # Initial value of R**2
+    r_square = 1 - (mean_squared_error/mean_squared_error_y)
+    print("Value of R square initially: ",r_square)
 
     b, m = gradient_descent(x, y, learning_rate,
                             initial_b, initial_m, number_of_itr)
+
+    # Mean squared error at the end
+    mean_squared_error = compute_error_for_given_points(
+        b, m, x, y)
+    
+    # Final value of R**2
+    r_square = 1 - (mean_squared_error/mean_squared_error_y)
+    print("Value of R square finally: ",r_square)
 
     print('Ending gradient descent at b = {1}, m = {2}, error = {3} with {0} iterations'.format(
         number_of_itr, b, m, compute_error_for_given_points(b, m, x, y)))
@@ -54,6 +87,28 @@ def main():
     plt.scatter(x, y)
     plt.plot(x, x*m + b, 'r')
     plt.show()
+
+
+def sklearn_linear_regression(data):
+    x = np.array(data.iloc[:, :-1])
+    y = np.array(data.iloc[:, 1:])
+
+    x = x.reshape(-1, 1)
+    y = y.reshape(-1, 1)
+
+    reg = linear_model.LinearRegression()
+    reg = reg.fit(x, y)
+    plt.scatter(x, y)
+    plt.plot(x, reg.predict(x))
+    plt.show()
+
+
+def main():
+    data = pd.read_csv(
+        "d:/Documents/Sem8/ML/1. Linear_Regression/data_back_pain.csv", sep=',', header=0)
+    manual_linear_regression(data)
+    sklearn_linear_regression(data)
+
 
 if __name__ == "__main__":
     main()
--- ML/Assignment 2/DecisionTree.py
+++ ML/Assignment 2/DecisionTree.py
@@ -1,51 +1,69 @@
+import os
+os.environ["PATH"] += os.pathsep + "D:\Program Files (x86)\Graphviz\Bin"
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
 from sklearn import tree
+from sklearn import preprocessing
 import graphviz
 
-#1.reading dataset
-data=pd.read_csv("ass2data.csv")
-features=data.columns
-features=features[1:-1]
-class_names=list(set(data.iloc[1:,-1]))
-#print(data)
+# 1. Reading dataset
+data = pd.read_csv("d:\Documents\Sem8\ML\Assignment 2\Ass2data.csv", sep=',')
+features = data.columns
+features = features[1:-1]
+class_names = list(set(data.iloc[1:, -1]))
 
-#2.printing the columns which are categorical string types
-#for i in data.columns.values:
-    #print(data[i].dtypes)
+print("Dataset Length: ", len(data))
+print("Dataset Shape: ", data.shape)
 
-#3.Converting string categorical columns into numerical categorical columns
+print("Dataset: ")
+print(data.head())
+
+# # 2. Printing the columns which are categorical string types
+# for i in data.columns.values:
+#     print(data[i].dtypes)
+
+# 3.Converting string categorical columns into numerical categorical columns
 for i in data.columns.values:
-    le=preprocessing.LabelEncoder()
-    if data[i].dtypes=="object":
+    le = preprocessing.LabelEncoder()
+    if data[i].dtypes == "object":
         le.fit(data[i])
-        data[i]=le.transform(data[i])
-#print(data)
+        data[i] = le.transform(data[i])
+print(data)
 
-#4.Splitting data into training and testing
-X_train=data.values[0:14,1:5]
-#print(X_train)
-Y_train=data.values[0:14,5]
-#print(Y_train)
-X_test=data.values[14,1:5]
-#print(X_test)
+X = data.values[:, 0:4]
+y = data.values[:, 5]
 
-#5.DecisionTree using gini index
-# clf_gini=DecisionTreeClassifier(criterion="gini")
-# clf_gini.fit(X_train,Y_train)
-# output1=clf_gini.predict([X_test])
-# print(output1)
-# dot_data1=tree.export_graphviz(clf_gini,out_file=None,feature_names=features,class_names=class_names,filled=True,rounded=True,special_characters=True)
-# graph=graphviz.Source(dot_data1)
-# graph.render('DecisionTreeGini',view=True)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=100)
 
-#DecisionTree using entropy and IG
-clf_entropy=DecisionTreeClassifier(criterion="entropy")
-clf_entropy.fit(X_train,Y_train)
-output2=clf_entropy.predict([X_test])
-print(output2)
+clf_gini = DecisionTreeClassifier(criterion="gini")
+clf_gini.fit(X_train, y_train)
 
-dot_data2=tree.export_graphviz(clf_entropy,out_file=None,feature_names=features,class_names=class_names,filled=True,rounded=True,special_characters=True)
-graph=graphviz.Source(dot_data2)
-graph.render('DecisionTreeEntropy',view=True)
+clf_entropy = DecisionTreeClassifier(criterion="entropy")
+clf_entropy.fit(X_train, y_train)
+
+y_pred = clf_gini.predict(X_test)
+print("Accuracy of decision tree with Gini index is: ",
+      accuracy_score(y_test, y_pred)*100)
+
+print("\nPredictions by Gini index: ", y_pred)
+
+y_pred_en = clf_entropy.predict(X_test)
+print("Accuracy of decision tree with Entropy is: ",
+      accuracy_score(y_test, y_pred_en)*100)
+
+print("\nPredictions by Entropy: ", y_pred_en)
+
+dot_data1 = tree.export_graphviz(clf_gini, out_file=None, feature_names=features,
+                                 class_names=class_names, filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data1)
+graph.render('DecisionTreeGini', view=True)
+
+
+dot_data2 = tree.export_graphviz(clf_entropy, out_file=None, feature_names=features,
+                                 class_names=class_names, filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data2)
+graph.render('DecisionTreeEntropy', view=True)
